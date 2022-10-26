4cbef2b7e5e938b762aee152347fa0d5861477a6
DistilledCode <107433905+DistilledCode@users.noreply.github.com>
2022-10-26 11:09:02 +0530

    performance optimization: instances lookup in ItemAnalytics.summarize() is ~4x faster on cost of slighlty slower __init__(); ItemAnalytics._is_valid_id() now has constant time complexity

--- src/ambrosial/swan/items.py
+++ src/ambrosial/swan/items.py
@@ -1,4 +1,5 @@
 from collections import Counter, defaultdict
+from itertools import groupby
 from typing import Any, Optional
 
 import ambrosial.swan.typealiases as alias
@@ -10,6 +11,7 @@ class ItemAnalytics:
     def __init__(self, swiggy: Swiggy) -> None:
         self.swiggy = swiggy
         self.all_items = self.swiggy.get_items()
+        self.items_map = self._get_items_map()
 
     def group(self) -> dict[Item, int]:
         return dict(Counter(self.all_items).most_common())
@@ -41,19 +43,20 @@ class ItemAnalytics:
         item_id: Optional[int] = None,
     ) -> dict[int, list[alias.History]]:
         if item_id is not None and self._is_valid_id(item_id):
-            item_list = [item for item in self.all_items if item.item_id == item_id]
+            item_map = {item_id: self.items_map[item_id]}
         else:
-            item_list = self.all_items
-        hist = defaultdict(list)
-        for item in item_list:
-            hist[item.item_id].append(
+            item_map = self.items_map
+        return {
+            item_id: [
                 alias.History(
                     order_id=item.order_id,
                     address_id=self.swiggy.get_order(item.order_id).address.address_id,
                     order_time=self.swiggy.get_order(item.order_id).order_time,
                 )
-            )
-        return dict(hist)
+                for item in items
+            ]
+            for item_id, items in item_map.items()
+        }
 
     def summarise(self, item_id: int) -> alias.Summarise:
         """
@@ -62,7 +65,7 @@ class ItemAnalytics:
         Those are calculated for each order.
         """
         self._is_valid_id(item_id)
-        instances = [item for item in self.all_items if item.item_id == item_id]
+        instances = self.items_map[item_id]
         total_quantity = 0
         total_base_price = 0.0
         total_base_price = 0.0
@@ -72,7 +75,6 @@ class ItemAnalytics:
         received_for_free = 0
         for item in instances:
             total_quantity += item.quantity - item.free_item_quantity
-            total_base_price += item.base_price * item.quantity
             total_base_price += item.subtotal
             total_discount += item.item_total_discount
             total_actual_cost += item.effective_item_price
@@ -101,9 +103,9 @@ class ItemAnalytics:
             ]
 
     def _is_valid_id(self, item_id: int) -> bool:
-        if item_id in [item.item_id for item in self.all_items]:
+        if item_id in self.items_map:
             return True
-        raise ValueError(f"order item of id = {repr(item_id)} does not exist.")
+        raise ValueError(f"item with id = {repr(item_id)} does not exist.")
 
     def _get_attr_detail(self, attr: str) -> list[tuple[Any, int]]:
         return Counter(getattr(item, attr) for item in self.all_items).most_common()
@@ -122,3 +124,7 @@ class ItemAnalytics:
         return Counter(
             item.category_details["category"] for item in self.all_items
         ).most_common()
+
+    def _get_items_map(self) -> dict[int, list[Item]]:
+        items = sorted(self.all_items, key=lambda x: x.item_id)
+        return {item.item_id: list(grped_vals) for item, grped_vals in groupby(items)}
