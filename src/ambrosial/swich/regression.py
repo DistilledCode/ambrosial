1cc9564f712a625adbf4f2666ccba76858e698de
DistilledCode <107433905+DistilledCode@users.noreply.github.com>
2022-11-01 20:33:15 +0530

    minor visual tweaks

--- src/ambrosial/swich/regression.py
+++ src/ambrosial/swich/regression.py
@@ -1,17 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+from july.rcmod import update_rcparams
 from palettable.lightbartlein import diverging
 
 from ambrosial.swan import SwiggyAnalytics
 from ambrosial.swich.helper.regression import get_dataframe
 from ambrosial.swich.utils import CustomFormatter, CustomLocator
 
+BINS = "per_minute_"
+
 
 class RegressionPlot:
     def __init__(self, swan: SwiggyAnalytics) -> None:
         self.swan = swan
-        self.cmap = "rainbow"
         self.cmap = diverging.BlueOrange8_8_r.mpl_colormap
         self.scatter_kws_cb = {
             "color": None,
@@ -29,7 +31,8 @@ class RegressionPlot:
         self.line_kws = {"color": "tomato", "linewidth": 3.0}
         sns.set_theme(style="ticks")
 
-    def order_amount(self, bins: str) -> None:
+    def order_amount(self, bins: str = BINS) -> None:
+        update_rcparams(xmargin=0.05, ymargin=0.05, titlepad=20)
         df = get_dataframe(code="oa", swan=self.swan, bins=bins)
 
         p = sns.regplot(
@@ -50,7 +53,12 @@ class RegressionPlot:
         p.set_xlabel("")
         p.set_ylabel("Order Total    (in ₹)", fontsize=15)
 
-    def ordamt_ordfeeprcnt(self, bins: str, remove_outliers: bool = True) -> None:
+    def ordamt_ordfeeprcnt(
+        self,
+        bins: str = BINS,
+        remove_outliers: bool = True,
+    ) -> None:
+        update_rcparams(xmargin=0.05, ymargin=0.05, titlepad=20)
         df = get_dataframe(code="oa_ofp", swan=self.swan, bins=bins, ro=remove_outliers)
         p = sns.regplot(
             data=df,
@@ -64,12 +72,13 @@ class RegressionPlot:
             scatter_kws=self.scatter_kws,
         )
         p.set_title(f'Fee Percentage v/s Order Total\nbins="{bins}"', fontsize=15)
-        p.set_xticks([int(i) for i in np.linspace(min(df["x"]), max(df["x"]), 20)])
-        p.set_yticks([float(i) for i in np.linspace(min(df["y"]), max(df["y"]), 20)])
+        p.set_xticks([int(i) for i in np.linspace(min(df["x"]), max(df["x"]), 10)])
+        p.set_yticks([float(i) for i in np.linspace(min(df["y"]), max(df["y"]), 10)])
         p.set_ylabel("Fee as % of Order Total\n", fontsize=15)
         p.set_xlabel("Order Total    (in ₹)", fontsize=15)
 
-    def ordamt_ordfee(self, bins: str, remove_outliers: bool = True) -> None:
+    def ordamt_ordfee(self, bins: str = BINS, remove_outliers: bool = True) -> None:
+        update_rcparams(xmargin=0.05, ymargin=0.05, titlepad=20)
         df = get_dataframe(code="oa_of", swan=self.swan, bins=bins, ro=remove_outliers)
         p = sns.regplot(
             data=df,
@@ -83,8 +92,8 @@ class RegressionPlot:
             scatter_kws={**self.scatter_kws_cb, "c": df["color"], "cmap": self.cmap},
         )
         p.set_title(f'Order Total v/s Order Fee\nbins="{bins}"', fontsize=15)
-        p.set_xticks([int(i) for i in np.linspace(min(df["x"]), max(df["x"]), 20)])
-        p.set_yticks([int(i) for i in np.linspace(min(df["y"]), max(df["y"]), 20)])
+        p.set_xticks([int(i) for i in np.linspace(min(df["x"]), max(df["x"]), 10)])
+        p.set_yticks([int(i) for i in np.linspace(min(df["y"]), max(df["y"]), 10)])
         p.set_xlabel("Order Fee    (in ₹)", fontsize=15)
         p.set_ylabel("Order Total    (in ₹)", fontsize=15)
         sm = plt.cm.ScalarMappable(cmap=self.cmap)
@@ -93,7 +102,7 @@ class RegressionPlot:
         cbar = p.figure.colorbar(
             sm,
             ax=ax,
-            ticks=CustomLocator(ticks=20),
+            ticks=CustomLocator(ticks=10),
             format=CustomFormatter(vmin=min(df["color"]), vmax=max(df["color"])),
         )
         cbar.set_label(
@@ -107,6 +116,7 @@ class RegressionPlot:
         # robust and order are mutually exclusive
 
     def ordtime_orddist(self, remove_outliers: bool = True) -> None:
+        update_rcparams(xmargin=0.05, ymargin=0.05, titlepad=20)
         df = get_dataframe(code="ot_od", swan=self.swan, ro=remove_outliers)
         p = sns.regplot(
             data=df,
@@ -126,7 +136,7 @@ class RegressionPlot:
                 for i in np.linspace(
                     min(df["x"]),
                     max(df["x"]),
-                    20,
+                    10,
                 )
             ]
         )
@@ -136,7 +146,7 @@ class RegressionPlot:
                 for i in np.linspace(
                     min(df["y"]),
                     max(df["y"]),
-                    20,
+                    10,
                 )
             ]
         )
@@ -144,6 +154,7 @@ class RegressionPlot:
         p.set_ylabel("Order Delivery Time  (minutes)\n", fontsize=15)
 
     def ordtime_punctuality_bool(self, remove_outliers: bool = True) -> None:
+        update_rcparams(xmargin=0.05, ymargin=0.05, titlepad=20)
         df = get_dataframe(code="ot_p_b", swan=self.swan, ro=remove_outliers)
         p = sns.regplot(
             data=df,
@@ -165,7 +176,7 @@ class RegressionPlot:
                 for i in np.linspace(
                     min(df["x"]),
                     max(df["x"]),
-                    20,
+                    10,
                 )
             ]
         )
@@ -175,6 +186,7 @@ class RegressionPlot:
         p.set_ylabel("Was Order On Time\n", fontsize=15)
 
     def orddist_punctuality_bool(self, remove_outliers: bool = True) -> None:
+        update_rcparams(xmargin=0.05, ymargin=0.05, titlepad=20)
         df = get_dataframe(code="od_p_b", swan=self.swan, ro=remove_outliers)
         p = sns.regplot(
             data=df,
@@ -196,7 +208,7 @@ class RegressionPlot:
                 for i in np.linspace(
                     min(df["x"]),
                     max(df["x"]),
-                    20,
+                    10,
                 )
             ]
         )
@@ -206,6 +218,7 @@ class RegressionPlot:
         p.set_ylabel("Was Order On Time\n", fontsize=15)
 
     def ordtime_punctuality(self, remove_outliers: bool = True) -> None:
+        update_rcparams(xmargin=0.05, ymargin=0.05, titlepad=20)
         df = get_dataframe(code="ot_p", swan=self.swan, ro=remove_outliers)
         p = sns.regplot(
             data=df,
@@ -233,13 +246,13 @@ class RegressionPlot:
         cbar = p.figure.colorbar(
             sm,
             ax=ax,
-            ticks=CustomLocator(ticks=15),
+            ticks=CustomLocator(ticks=10),
             format=CustomFormatter(vmax=max(df["color"]), vmin=min(df["color"])),
         )
         cbar.set_label(
             """
-                        SLA
-                        Difference
+                SLA
+                Difference
             """,
             size=15,
             rotation="horizontal",
